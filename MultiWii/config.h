2d30a7645e872d3c6f9caf8bef5aeab2252edb88
Alex Spencer <alexedwardspencer@gmail.com>
2014-05-12 19:57:15 +0100

    Removed LPF

--- MultiWii/config.h
+++ MultiWii/config.h
@@ -499,7 +499,7 @@
          balancing options ran out. Uncomment only one option!
          IMPORTANT! Change low pass filter setting changes PID behaviour, so retune your PID's after changing LPF.*/
       //#define MPU6050_LPF_256HZ     // This is the default setting, no need to uncomment, just for reference
-      #define MPU6050_LPF_188HZ
+      //#define MPU6050_LPF_188HZ
       //#define MPU6050_LPF_98HZ
       //#define MPU6050_LPF_42HZ
       //#define MPU6050_LPF_20HZ
